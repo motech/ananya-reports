d156be8db567b19d850dc874e6cb2ffb0037ec8c
Vignesh R E <vignesh.littlemaster@gmail.com>
2012-07-02 17:30:27 +0530

    V2, Koti | #1738 | making SpringIntegrationTest transactional

--- ananya-reports-domain/src/test/java/org/motechproject/ananya/kilkari/reports/repository/SpringIntegrationTest.java
+++ ananya-reports-domain/src/test/java/org/motechproject/ananya/kilkari/reports/repository/SpringIntegrationTest.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationKilkariReportsContext.xml")
+@Transactional
 public abstract class SpringIntegrationTest {
 
     @Autowired
