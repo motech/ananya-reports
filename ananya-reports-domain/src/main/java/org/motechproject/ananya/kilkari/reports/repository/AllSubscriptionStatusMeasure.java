737cf667f8c5717b7e30aaa52a0625089403167d
Aravind M <arav.psgtech@gmail.com>
2012-07-19 17:06:47 +0530

    Aravind | #00 | Added transactional annotation

--- ananya-reports-domain/src/main/java/org/motechproject/ananya/kilkari/reports/repository/AllSubscriptionStatusMeasure.java
+++ ananya-reports-domain/src/main/java/org/motechproject/ananya/kilkari/reports/repository/AllSubscriptionStatusMeasure.java
@@ -5,10 +5,12 @@ import org.hibernate.criterion.Restrictions;
 import org.motechproject.ananya.kilkari.reports.domain.measure.SubscriptionStatusMeasure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public class AllSubscriptionStatusMeasure {
     @Autowired
     private DataAccessTemplate template;
