8c93719c7944309516ff431537efd68fc47e6683
Aravind M <arav.psgtech@gmail.com>
2012-07-20 16:54:29 +0530

    Sneha/Aravind | #1740 | Added transactional to subscription service

--- ananya-reports-domain/src/main/java/org/motechproject/ananya/kilkari/reports/service/SubscriptionService.java
+++ ananya-reports-domain/src/main/java/org/motechproject/ananya/kilkari/reports/service/SubscriptionService.java
@@ -4,10 +4,12 @@ import org.motechproject.ananya.kilkari.reports.domain.dimension.Subscription;
 import org.motechproject.ananya.kilkari.reports.repository.AllSubscriptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class SubscriptionService {
 
     private AllSubscriptions allSubscriptions;
