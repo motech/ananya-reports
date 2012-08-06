53d08fced2312afeaf15078f5e8e0b282f690f9a
Srivatsa Katta <vatsa.katta@gmail.com>
2012-08-06 16:58:03 +0530

    Katta|000|Added smoke tests in reporting module

---
+++ ananya-reports-smoke/src/test/java/org/motechproject/ananya/reports/smoke/TimedRunner.java
@@ -0,0 +1,38 @@
+package org.motechproject.ananya.reports.smoke;
+
+public abstract class TimedRunner<T> {
+
+    private int tries;
+    private int intervalSleep;
+
+    public TimedRunner() {
+        this(5, 1000);
+    }
+
+    public TimedRunner(int tries, int intervalSleep) {
+        this.tries = tries;
+        this.intervalSleep = intervalSleep;
+    }
+
+    /*
+    * Function to run within the timeout. It returns a boolean. If the value is true, the code
+    * will break out of the loop immediately else try again within the timeout period.
+    */
+    protected abstract T run();
+
+    public T executeWithTimeout() {
+        T result = null;
+        for (int i = 0; i < tries; i++) {
+            result = run();
+            if (result != null) break;
+
+            try {
+                Thread.sleep(intervalSleep);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread was interrupted.", e);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ ananya-reports-smoke/src/test/java/org/motechproject/ananya/reports/smoke/contract/SubscriptionRequest.java
@@ -0,0 +1,76 @@
+package org.motechproject.ananya.reports.smoke.contract;
+
+import org.joda.time.DateTime;
+
+import java.util.UUID;
+
+public class SubscriptionRequest {
+    private String subscriptionId;
+    private String channel;
+    private Long msisdn;
+    private String pack;
+    private String subscriptionStatus;
+    private DateTime createdAt;
+    private DateTime startDate;
+
+
+    public SubscriptionRequest() {
+        subscriptionId = UUID.randomUUID().toString();
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public Long getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(Long msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public String getPack() {
+        return pack;
+    }
+
+    public void setPack(String pack) {
+        this.pack = pack;
+    }
+
+    public String getSubscriptionStatus() {
+        return subscriptionStatus;
+    }
+
+    public void setSubscriptionStatus(String subscriptionStatus) {
+        this.subscriptionStatus = subscriptionStatus;
+    }
+
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+}
