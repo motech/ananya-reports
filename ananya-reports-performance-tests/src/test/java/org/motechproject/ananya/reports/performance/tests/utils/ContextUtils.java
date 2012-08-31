b436c970817b0b30654fd0dfbdd9e4cac5a61a7d
Ashish Mishra <ashishkm@thoughtworks.com>
2012-08-31 13:58:29 +0530

    Ashish | #00 | Ensuring that exceptions are logged while instantiating ContextUitls class

--- ananya-reports-performance-tests/src/test/java/org/motechproject/ananya/reports/performance/tests/utils/ContextUtils.java
+++ ananya-reports-performance-tests/src/test/java/org/motechproject/ananya/reports/performance/tests/utils/ContextUtils.java
@@ -4,10 +4,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContextUtils {
 
-    private static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationReportsPerformanceContext.xml");
+    private static ClassPathXmlApplicationContext context;
+
+    static {
+        try {
+            context = new ClassPathXmlApplicationContext("applicationReportsPerformanceContext.xml");
+        } catch (Exception e) {
+            System.err.println("Error while initializing spring context: " + e.getMessage());
+            e.printStackTrace();
+            throw e;
+        }
+    }
 
     public static BaseConfiguration getConfiguration(){
         return (BaseConfiguration)context.getBean("baseConfiguration");
-
     }
+
 }
