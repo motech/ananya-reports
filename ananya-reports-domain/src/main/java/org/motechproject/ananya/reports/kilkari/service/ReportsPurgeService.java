742b996fa9599f83332707b924f7b8f992be85bf
Aravind M <arav.psgtech@gmail.com>
2012-09-21 12:42:20 +0530

    Ashok/Aravind | #1774 | Loggin reports purging.

--- ananya-reports-domain/src/main/java/org/motechproject/ananya/reports/kilkari/service/ReportsPurgeService.java
+++ ananya-reports-domain/src/main/java/org/motechproject/ananya/reports/kilkari/service/ReportsPurgeService.java
@@ -1,5 +1,7 @@
 package org.motechproject.ananya.reports.kilkari.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ public class ReportsPurgeService {
     private SubscriberCallMeasureService subscriberCallMeasureService;
     private SubscriptionStatusMeasureService subscriptionStatusMeasureService;
     private SubscriptionService subscriptionService;
+    private final Logger logger = LoggerFactory.getLogger(ReportsPurgeService.class);
 
     @Autowired
     public ReportsPurgeService(SubscriberCallMeasureService subscriberCallMeasureService, SubscriptionStatusMeasureService subscriptionStatusMeasureService, SubscriptionService subscriptionService) {
@@ -17,9 +20,11 @@ public class ReportsPurgeService {
     }
 
     public void purge(String msisdn) {
+        logger.info("Started purging report records for msisdn : " + msisdn);
         Long msisdnInLong = Long.valueOf(msisdn);
         subscriberCallMeasureService.deleteFor(msisdnInLong);
         subscriptionStatusMeasureService.deleteFor(msisdnInLong);
         subscriptionService.deleteFor(msisdnInLong);
+        logger.info("Finished purging report records for msisdn : " + msisdn);
     }
 }
