1b51ac8e4ce4a570c2d350eb7e19322ef86e1aee
Sharvani <sharvani.n.s@gmail.com>
2013-02-14 11:57:01 +0530

    Vishnu,Sharvani |#00 | Fixing buid.

--- ananya-reports-migrations/src/main/resources/db/migration/V1_47.sql
+++ ananya-reports-migrations/src/main/resources/db/migration/V1_47.sql
@@ -1,5 +1,3 @@
-ALTER TABLE report.subscription_pack_dimension ADD COLUMN subscription_pack_alias VARCHAR(255);
-
 UPDATE report.subscription_pack_dimension
 SET subscription_pack_alias = '16 Month'
 WHERE subscription_pack = 'BARI_KILKARI';
