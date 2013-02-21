b19176028a3e0e0dc9bb113ce412a1510f7a8cf5
vishnu karthik <karthikl@thoughtworks.com>
2013-02-21 16:22:02 +0530

    Vishnu, Hari | #15 | Changing the index

--- ananya-reports-migrations/src/main/resources/db/migration/V1_48.sql
+++ ananya-reports-migrations/src/main/resources/db/migration/V1_48.sql
@@ -1,15 +1,14 @@
-CREATE INDEX status_idx
-  ON report.subscription_status_measure
-  USING btree
-  (status COLLATE pg_catalog."default");
+CREATE INDEX subscriptionId_status
+ON report.subscription_status_measure
+USING btree
+(subscription_id, status COLLATE pg_catalog."default");
 
-CREATE INDEX "subscriptionIdx"
-  ON report.subscription_status_measure
-  USING btree
-  (last_modified_time, subscription_id);
+CREATE INDEX lastModifiedTime_subscriptionId
+ON report.subscription_status_measure
+USING btree
+(last_modified_time,subscription_id);
 
 CREATE INDEX msisdn_idx
   ON report.subscriptions
   USING btree
   (msisdn);
-
