0077849f9d5e99a2fc6ec7abec63cd2e3c4ddbb4
Vishal Naik <vishalnaik@thoughtworks.com>
2013-05-22 15:42:48 +0530

    Vishal #0 Removing setting default state to Bihar in the Location_Dimension.state column

--- ananya-reports-migrations/src/main/resources/db/migration/V1_53.sql
+++ ananya-reports-migrations/src/main/resources/db/migration/V1_53.sql
@@ -1,4 +1,4 @@
-ALTER TABLE report.location_dimension ADD COLUMN state VARCHAR(255) DEFAULT 'BIHAR';
+ALTER TABLE report.location_dimension ADD COLUMN state VARCHAR(255);
 UPDATE report.location_dimension set state = 'BIHAR';
 ALTER TABLE report.location_dimension alter COLUMN state set NOT NULL;
 ALTER TABLE report.location_dimension DROP CONSTRAINT unq_district_block_panchayat;
