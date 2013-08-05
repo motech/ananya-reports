76753e874a3a18f952d2079da7a97e9621d911e6
Vinod John <johnvic@thoughtworks.com>
2013-08-05 15:57:16 +0530

    Revert "Revert "Vishal #0 Updating default state case to camel-case""
    This reverts commit be995c8be1bb458fe554db8ee39d806aa1f91bc0.

--- ananya-reports-migrations/src/main/resources/db/migration/V1_53.sql
+++ ananya-reports-migrations/src/main/resources/db/migration/V1_53.sql
@@ -1,5 +1,5 @@
 ALTER TABLE report.location_dimension ADD COLUMN state VARCHAR(255);
-UPDATE report.location_dimension set state = 'BIHAR';
+UPDATE report.location_dimension set state = 'Bihar';
 ALTER TABLE report.location_dimension alter COLUMN state set NOT NULL;
 ALTER TABLE report.location_dimension DROP CONSTRAINT unq_district_block_panchayat;
 ALTER TABLE report.location_dimension ADD CONSTRAINT unq_state_district_block_panchayat UNIQUE (state, district, block, panchayat);
