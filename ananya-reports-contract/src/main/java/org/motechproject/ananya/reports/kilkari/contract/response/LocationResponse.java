d50f8950faad5eae56e60632958225bd6c729ff9
Vinod John <johnvic@thoughtworks.com>
2013-08-05 15:56:31 +0530

    Revert "Revert "Vishal, Shashank #0 Adding state to unique constraint""
    This reverts commit 688554627673bcd31b4f312113a1dc93d3bf665d.

--- ananya-reports-contract/src/main/java/org/motechproject/ananya/reports/kilkari/contract/response/LocationResponse.java
+++ ananya-reports-contract/src/main/java/org/motechproject/ananya/reports/kilkari/contract/response/LocationResponse.java
@@ -40,6 +40,10 @@ public class LocationResponse {
         return district;
     }
 
+    public String getState() {
+        return state;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
